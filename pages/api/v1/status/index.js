601944ef0845c73f11981c56434f57bfc7c0b91b
Alison Souza <alison@contasimples.com>
2024-06-18 00:43:34 -0300

    implementa  e

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,10 @@
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as row;");
+
+  console.log({ result: result.rows });
+  response.status(200).json({ chave: "são acima da média" });
+}
+
+export default status;
