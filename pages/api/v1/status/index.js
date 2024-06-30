5195e249d0ea7ddf4d52e33b985ff80b3715a25e
Alison Souza <alison@contasimples.com>
2024-06-30 19:13:39 -0300

    refactor: atualiza query afim de evitar vulnerabilidades

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,34 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as row;");
+  const result = await database.query("SHOW server_version;");
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
 
-  console.log({ result: result.rows });
-  response.status(200).json({ chave: "são acima da média" });
+  const databaseName = process.env.POSTGRES_DB;
+  const openConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+
+  const versionString = result.rows[0].server_version;
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+  const openConnections = openConnectionsResult.rows[0].count;
+
+  const versionMatch = versionString.match(/(\d+\.\d+)/);
+  const version = versionMatch ? versionMatch[1] : null;
+
+  const createdAt = new Date().toISOString();
+
+  response.status(200).json({
+    created_at: createdAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(maxConnections),
+        opened_connextions: openConnections,
+        version: version,
+      },
+    },
+  });
 }
 
 export default status;
